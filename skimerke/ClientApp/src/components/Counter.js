fea009c5aab629f0647ee6f07df86282da903178
terjetop <terjetop@uia.no>
2024-03-26 10:29:57 +0100

    check if renaming repo causes any trouble

--- skimerke/ClientApp/src/components/Counter.js
+++ skimerke/ClientApp/src/components/Counter.js
@@ -19,7 +19,6 @@ export class Counter extends Component {
     return (
       <div>
         <h1>Counter</h1>
-
         <p>This is a simple example of a React component.</p>
 
         <p aria-live="polite">Current count: <strong>{this.state.currentCount}</strong></p>
