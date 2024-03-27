f6603629439aefd188f4ba271b085d2838df53ae
terjetop <terjetop@uia.no>
2024-03-27 08:58:09 +0100

    Revert "this commit was not as intended"
    This reverts commit 8bdf66677175778ec45cc2eb0525792ac642e0ca.

--- skimerke/Program.cs
+++ skimerke/Program.cs
@@ -23,7 +23,7 @@ builder.Services.AddAuthentication()
     .AddIdentityServerJwt();
 
 builder.Services.AddControllersWithViews();
-builder.Services.AddRazorPages(); 
+builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
