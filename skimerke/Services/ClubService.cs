540b2f492d76cf74a46fabbedcf0ea927d22e3ab
terjetop <terjetop@uia.no>
2024-04-23 08:49:48 +0200

    Added Service in ClubService to add a new club

--- skimerke/Services/ClubService.cs
+++ skimerke/Services/ClubService.cs
@@ -6,6 +6,30 @@ namespace skimerke.Services;
 
 public class ClubService(ApplicationDbContext context) : IClubService
 {
+    public async Task<Club> AddClub(Club addedClub)
+    {
+        var clubExists = await context.Clubs.AnyAsync(c => c.OrganizationNumberId == addedClub.OrganizationNumberId);
+        if (clubExists)
+        {
+            throw new ArgumentException($"Club with {addedClub.OrganizationNumberId} already exists");
+        }
+
+        var club = new Club
+        {
+            ClubName = addedClub.ClubName,
+            Address = addedClub.Address,
+            City = addedClub.City,
+            PostalCode = addedClub.PostalCode,
+            EMail = addedClub.EMail,
+            OrganizationNumberId = addedClub.OrganizationNumberId
+        };
+
+        context.Clubs.Add(club);
+        await context.SaveChangesAsync();
+
+        return club;
+    }
+
     public async Task<IEnumerable<Club>> GetAllClubs()
     {
         var clubs =  await context.Clubs.ToListAsync();
--- skimerke/Services/IClubService.cs
+++ skimerke/Services/IClubService.cs
@@ -4,6 +4,7 @@ namespace skimerke.Services;
 
 public interface IClubService
 {
+    Task<Club> AddClub(Club addedClub);
     Task<IEnumerable<Club>> GetAllClubs();
     Task<Club?> GetClubById(int id);
 
